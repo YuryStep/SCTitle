a9286efc82b7afdc5ff3afa2c668152e92324dad
Yury Stepanchuk <stich-style@mail.ru>
2023-11-22 11:45:33 +0300

    Initial Commit

---
+++ SCTitle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SCTitle
+//
+//  Created by Юрий Степанчук on 22.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
